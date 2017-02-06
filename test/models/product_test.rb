715ef0e1a488342d7a9e168de7384f53a5c1cf4a
katya-danilochkina <katya30108611@gmail.com>
2017-02-06 20:01:34 +0200

    Add checks for price

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -10,4 +10,24 @@ class ProductTest < ActiveSupport::TestCase
   assert product.errors[:price].any?
   assert product.errors[:image_url].any?
   end
+
+  test "product price must be positive" do
+  	# цена товара должна быть положительной
+  	product = Product.new(title: "My Book Title", 
+  	                      description:  "yyy",
+  	                      image_url: "zzz.jpg")
+  	product.price = -1
+  	assert product.invalid?
+  	assert_equal ["must be greater than or equal to 0.01"],
+
+  	product.errors[:price]
+  	#должна быть больше или равна 0.01
+  	product.price = 0
+
+  	assert product.invalid?
+  	assert_equal ["must be greater than or equal to 0.01"],
+  	    product.errors[:price]
+  	product.price = 1
+  	assert product.valid?                          
+  end
 end
